b1503a8a9ffbcfb13bd7ba833bca034bfea2d428
Robot Ecology Lab <robotecologylab@uci.edu>
2023-06-13 17:49:18 -0700

    Changed folder name in docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,6 +2,6 @@ sudo docker run -d \
 	--net=host \
 	--restart=always \
 	--name=robotarium_vicon_tracker \
-	-v ~/git/vicon_tracker_python/config/node_desc_tracker.json:/code/descriptor/desc.json \
-	-v ~/git/vicon_tracker_python/tracker_node.py:/code/tracker_node.py \
+	-v ~/git/robotarium_vicon_tracker/config/node_desc_tracker.json:/code/descriptor/desc.json \
+	-v ~/git/robotarium_vicon_tracker/tracker_node.py:/code/tracker_node.py \
 	robotarium:vicon_tracker python3 /code/tracker_node.py -host 192.168.1.5 -port 1884 -interval 33 /code/descriptor/desc.json
